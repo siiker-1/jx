d189ee6162c601dbb897fea7f51f0c6b5158c2fe
siiker-1 <siiker-1@users.noreply.github.com>
2026-09-07 22:44:42 +0800

    fix: escape PocketBay iframe for map navigation

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'jiangxi-handbook-v6';
-const ASSETS = ['/', '/index.html', '/styles.css?v=6', '/app.js?v=6', '/manifest.webmanifest', '/icon.svg'];
+const CACHE = 'jiangxi-handbook-v7';
+const ASSETS = ['/', '/index.html', '/styles.css?v=7', '/app.js?v=7', '/manifest.webmanifest', '/icon.svg'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', e => {
